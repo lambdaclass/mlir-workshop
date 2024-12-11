a34d65e8f1a5eb235a94d6a001a350b2e90c3447
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-12-11 13:06:03 -0300

    Implement ifelse_stmt build

--- src/codegen/ifelse_stmt.rs
+++ src/codegen/ifelse_stmt.rs
@@ -1,10 +1,13 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::Deref};
 
-use melior::ir::{Block, Value};
+use melior::{
+    dialect::scf,
+    ir::{Block, Location, Region, Value},
+};
 
 use crate::ast::IfStmt;
 
-use super::ModuleCtx;
+use super::{compile_statement, expressions::compile_expr, ModuleCtx};
 
 pub fn compile_if<'ctx, 'parent>(
     ctx: &ModuleCtx<'ctx>,
@@ -12,5 +15,39 @@ pub fn compile_if<'ctx, 'parent>(
     block: &'parent Block<'ctx>,
     stmt: &IfStmt,
 ) {
-    todo!()
+    let cond_value = compile_expr(ctx, locals, block, &stmt.cond);
+
+    block.append_operation(scf::r#if(
+        cond_value,
+        &[],
+        {
+            let mut locals = locals.clone();
+            let region = Region::new();
+            let block = region.append_block(Block::new(&[]));
+
+            for stmt in &stmt.then.stmts {
+                compile_statement(ctx, &mut locals, block.deref(), stmt);
+            }
+
+            block.append_operation(scf::r#yield(&[], Location::unknown(&ctx.ctx)));
+
+            region
+        },
+        {
+            let region = Region::new();
+            let block = region.append_block(Block::new(&[]));
+
+            if let Some(else_block) = &stmt.r#else {
+                let mut locals = locals.clone();
+                for stmt in &else_block.stmts {
+                    compile_statement(ctx, &mut locals, block.deref(), stmt);
+                }
+            }
+
+            block.append_operation(scf::r#yield(&[], Location::unknown(&ctx.ctx)));
+
+            region
+        },
+        Location::unknown(&ctx.ctx),
+    ));
 }
